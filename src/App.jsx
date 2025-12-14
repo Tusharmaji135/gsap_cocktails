b6dc1da3f7c29bf75a1ac0f2943692c124e46873
Tushar <135tusharmaji@gmail.com>
2025-12-14 16:44:38 +0530

    Hero section p1

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-blue-500">
+
+      </div>
     </main>
   );
 };
